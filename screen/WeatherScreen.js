60110dc4b861abad337fd7d35334d27fdf157cd8
unknown <6110110044@psu.ac.th>
2020-08-23 15:39:24 +0700

    finished part 6

---
+++ screen/WeatherScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Weather from '../components/Weather'
+import { View } from 'react-native'
+export default function WeatherScreen({route}){
+    return (
+        <View>
+            <Weather zipCode={route.params.zipCode}/>
+        </View>
+    )
+}
